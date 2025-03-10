2a50a564dde2f5132c6e2cb0bdb6616c4c4c6d8d
Gahel <music@gahel.fr>
2025-03-10 03:31:27 +0100

    wip: add center and circle handles

--- Assets/Scripts/PerspectiveMappingCamera.cs
+++ Assets/Scripts/PerspectiveMappingCamera.cs
@@ -32,8 +32,182 @@ public class PerspectiveMappingCamera : MonoBehaviour
     public float lineWidth = 0.02f;
     
     private bool _isFollowingMouse = false;
-    private int _actualIndex;
-    
+
+    class Handles {
+        public Handle none = new Handle();
+        public Corner[] corners = new Corner[4];
+        public CircleCorner[] circleCorners = new CircleCorner[4];
+        public Center center;
+
+        public Handle current;
+
+        public float magneticDistance = 0.2f;
+
+        private Handle[] all = new Handle[9]; // Corners, Center and Circle Corners
+
+        public void SelectNone() {
+            this.current = this.none;
+        }
+
+        public void SelectClosestHandle(Vector2 mousePos) {
+            this.current = GetClosestHandle(mousePos);
+        }
+
+        public Handles() {
+            int hIdx = 0;
+
+            for (int i = 0; i < corners.Length; i++) {
+                this.corners[i] = new Corner(Vector2.zero);
+                this.all[hIdx++] = this.corners[i];
+            }
+
+            this.center = new Center(this.corners);
+            this.all[hIdx++] = this.center;
+
+            for (int i = 0; i < this.corners.Length; i++) {
+                this.circleCorners[i] = new CircleCorner(
+                        this.corners[i],
+                        this.center,
+                        this.corners
+                        );
+                this.all[hIdx++] = this.circleCorners[i];
+            }
+
+            this.current = this.none;
+        }
+
+        public class Handle {
+            public HandleType type = HandleType.None;
+            protected Vector2 _position = Vector2.zero;
+
+            public virtual void SetPosition(Vector2 pos) {
+                _position = pos;
+            }
+            
+            public virtual Vector2 GetPosition() {
+                return _position;
+            }
+        }
+
+        public class Center: Handle {
+            private Corner[] _corners;
+
+            public Center(Corner[] corners) {
+                this.type = HandleType.Center;
+                this._corners = corners;
+            }
+
+            // translate all corners
+            public override void SetPosition(Vector2 pos) {
+                Vector2 translation = pos - GetPosition();
+                Debug.Log(translation);
+                foreach (Corner corner in _corners) {
+                    corner.SetPosition(corner.GetPosition() + translation);
+                }
+            }
+            
+            // FIXME: not a barycenter, diagonals meet point
+            public override Vector2 GetPosition() {
+                Vector2 barycenter = Vector2.zero;
+                foreach (Corner corner in this._corners) {
+                    barycenter += corner.GetPosition();
+                }
+                return barycenter / this._corners.Length;
+            }
+        }
+
+        public class Corner: Handle {
+            public Corner(Vector2 pos) {
+                this.type = HandleType.Corner;
+                this.SetPosition(pos);
+            }
+        }
+
+        public class CircleCorner: Handle {
+            private Corner _corner;
+            private Center _center;
+            private Corner[] _corners;
+
+            public CircleCorner(Corner corner, Center center, Corner[] corners) {
+                this.type = HandleType.CircleCorner;
+                this._corner = corner;
+                this._center = center;
+                this._corners = corners;
+            }
+
+            // set corresponding corner position
+            public override void SetPosition(Vector2 pos) {
+                Vector2[] circleCornerPos = new Vector2[4];
+                for (int i = 0; i < _corners.Length; i++) {
+                    if (_corners[i] == this._corner)
+                        circleCornerPos[i] = pos;
+                    else
+                        circleCornerPos[i] = GetCircleCornerPosition(_corners[i]);
+                }
+
+                // FIXME: not a barycenter, diagonals meet point
+                // new center will be the barycenter of corners
+                // which is also barycenter of circle corners
+                Vector2 barycenter = Vector2.zero;
+                foreach (Vector2 handlePos in circleCornerPos) {
+                    barycenter += handlePos;
+                }
+                Vector2 newCenterPos = barycenter / circleCornerPos.Length;
+
+                Vector2 cornerPos = newCenterPos + (pos-newCenterPos) * Mathf.Sqrt(2);
+                _corner.SetPosition(cornerPos);
+            }
+            
+            private Vector2 GetCircleCornerPosition(Corner corner) {
+                Vector2 centerPos = _center.GetPosition();
+                Vector2 cornerPos = corner.GetPosition();
+                return centerPos + (cornerPos-centerPos) * Mathf.Sqrt(2) / 2f;
+            }
+            
+            public override Vector2 GetPosition() {
+                Debug.Log( GetCircleCornerPosition(_corner));
+                return GetCircleCornerPosition(_corner);
+            }
+
+            public Corner GetCorner() {
+                return _corner;
+            }
+        }
+
+        public Handle GetClosestHandle(Vector2 mousePos) {
+            float minDist = Mathf.Infinity;
+            Handle closest = this.none;
+            foreach (Handle h in this.all) {
+                float dist = Vector2.Distance(h.GetPosition(), mousePos);
+                if (dist < minDist) {
+                    minDist = dist;
+                    closest = h;
+                }
+            }
+            if (minDist > magneticDistance) 
+                closest = this.none;
+
+            Debug.Log(closest.GetType());
+            return closest;
+        }
+
+        public void SetPosition(Handle handle, Vector2 pos) {
+            if (handle.type == HandleType.None)
+                return;
+
+            handle.SetPosition(pos);
+        }
+    }
+
+    public enum HandleType {
+        None,
+        Center,
+        Corner,
+        CircleCorner,
+    }
+
+    private Handles _handles = new Handles();
+
     private Vector3 _multiDisplayOffset;
 
     [HideInInspector] public Matrix4x4 matrix;
@@ -53,6 +227,7 @@ public class PerspectiveMappingCamera : MonoBehaviour
     }
 
     void Update() {
+        _handles.magneticDistance = magneticCornerDistance;
 
         if(Input.GetKeyDown(mappingModeHotkey)) {
             #if !UNITY_EDITOR
@@ -62,7 +237,6 @@ public class PerspectiveMappingCamera : MonoBehaviour
 
             if(interactable) {
                 _isFollowingMouse = false;
-                _actualIndex = -1;
                 SaveCorners();
             }
             interactable = !interactable;
@@ -101,7 +275,7 @@ public class PerspectiveMappingCamera : MonoBehaviour
 
         #if !UNITY_EDITOR
             if( GetMouseCurrentDisplay() != _cam.targetDisplay ) {
-                _actualIndex = -1;
+                _handles.SelectNone();
                 return;
             }
         #endif
@@ -112,20 +286,15 @@ public class PerspectiveMappingCamera : MonoBehaviour
             Debug.Log(mousePos);
             mousePos = remap(mousePos, 0f, 1f, -1f, 1f);
             // mousePos.y = -mousePos.y;
-            _actualIndex = GetClosestCornerIndex(mousePos);
-            Debug.Log(_actualIndex);
+            _handles.SelectClosestHandle(mousePos);
         }
 
-        // TODO: actualIndex -> enum with:
-        //     - corners
-        //     - center (for translations)
-        //     - circle intersections with diagonals
-        if( _actualIndex != -1) {
+        if( _handles.current.type != HandleType.None) {
             if( _isFollowingMouse) {
                 var mousePos = _cam.ScreenToViewportPoint(Input.mousePosition - _multiDisplayOffset);
                 mousePos = remap(mousePos, 0f, 1f, -1f, 1f);
                 // mousePos.y = -mousePos.y;
-                corners[_actualIndex] = mousePos;
+                _handles.current.SetPosition(mousePos);
             }
             else {
                
@@ -134,13 +303,14 @@ public class PerspectiveMappingCamera : MonoBehaviour
                 Vector2 delta = new Vector2( Input.GetAxisRaw( "Horizontal" ), Input.GetAxisRaw( "Vertical" ) * _cam.aspect ) * 0.1f;
                 if( Input.GetKey( KeyCode.LeftShift ) || Input.GetKey( KeyCode.RightShift ) ) delta *= 10;
                 else if(  Input.GetKey( KeyCode.LeftControl ) || Input.GetKey( KeyCode.RightControl ) ) delta *= 0.2f;
-                corners[ _actualIndex ] += delta * Time.deltaTime;
+                _handles.current.SetPosition(_handles.current.GetPosition() + delta * Time.deltaTime);
                     
             }
         }
 
         if(Input.GetMouseButtonUp(0)) {
              _isFollowingMouse = false;
+             _handles.SelectNone();
         }
     }
 
@@ -149,33 +319,18 @@ public class PerspectiveMappingCamera : MonoBehaviour
     }
 
     public void ResetCorners() {
-        _actualIndex = -1;
-
-        corners = new Vector2[4];
-        corners[1] = - Vector2.up - Vector2.right;
-        corners[0] = - Vector2.right + Vector2.up;
-        corners[3] = Vector2.up + Vector2.right;
-        corners[2] = Vector2.right - Vector2.up;
+        _handles.corners[1].SetPosition( - Vector2.up    - Vector2.right );
+        _handles.corners[0].SetPosition( - Vector2.right + Vector2.up    );
+        _handles.corners[3].SetPosition(   Vector2.up    + Vector2.right );
+        _handles.corners[2].SetPosition(   Vector2.right - Vector2.up    );
     }
 
     public Vector2[] GetCornerListForShaderVector2() {
-        return corners;
-    }
-
-    public int GetClosestCornerIndex(Vector2 mousePos) {
-        float minDist = Mathf.Infinity;
-        int closestIndex = -1;
-        for (int c = 0; c < corners.Length; c++) {
-            float dist = Vector2.Distance(corners[c], mousePos);
-            if (dist < minDist) {
-                minDist = dist;
-                closestIndex = c;
-            }
+        Vector2[] positions = new Vector2[4];
+        for (int i = 0; i < _handles.corners.Length; i++) {
+            positions[i] = _handles.corners[i].GetPosition();
         }
-        if(minDist > magneticCornerDistance) 
-            closestIndex = -1;
-
-        return closestIndex;
+        return positions;
     }
 
     public float GetMouseCurrentDisplay()
@@ -187,6 +342,7 @@ public class PerspectiveMappingCamera : MonoBehaviour
         return _cam.aspect;
     }
 
+    // TODO
     public void SaveCorners() {
         var config = new PerspectiveMappingDisplayConfig();
         config.corners = corners;
@@ -202,6 +358,7 @@ public class PerspectiveMappingCamera : MonoBehaviour
         Debug.Log("[PerspectiveMapping] Saved config file : " + path);
     }
 
+    // TODO
     public void LoadCorners() {
         var path = Path.Combine(Application.streamingAssetsPath, "PerspectiveMapping", "display" + _cam.targetDisplay + "_config.json");
         if(File.Exists(path)) {
