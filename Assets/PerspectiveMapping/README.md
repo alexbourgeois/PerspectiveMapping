cd89e5f9f848dfd5f5e529c103ee2f95e7232d45
alexbourgeois <contact@alexbourgeois.fr>
2025-08-31 19:18:53 +0200

    Added support for both input system (legacy and new)

--- Assets/PerspectiveMapping/README.md
+++ Assets/PerspectiveMapping/README.md
@@ -3,5 +3,7 @@
 /!\ Only compatible with Unity URP !
 
 Add PerspectiveMappingFeature to your renderer feature
+
 Add PerspectiveMappingCamera.cs script to your camera
+
 Add PerspectiveMappingUI.cs script to your camera to get UI for easier mapping
--- Assets/PerspectiveMapping/Runtime/PerspectiveMappingCamera.cs
+++ Assets/PerspectiveMapping/Runtime/PerspectiveMappingCamera.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 using System.IO;
 using System;
 
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
 [ExecuteInEditMode]
 [RequireComponent(typeof(Camera))]
 public class PerspectiveMappingCamera : MonoBehaviour
@@ -28,12 +32,18 @@ public class PerspectiveMappingCamera : MonoBehaviour
 
     private MappingInvariants _currentInvariants = MappingInvariants.Corners;
 
-    [Header("Controls settings")]
+    [Header("Perspective Mapping settings")]
     [SerializeField] public MappingInvariants mappingInvariants = MappingInvariants.Corners;
+
+    [Header("Controls settings")]
+#if ENABLE_INPUT_SYSTEM
+    public InputActionAsset inputActions;
+#else
     [SerializeField] public KeyCode mappingModeHotkey = KeyCode.P;
     [SerializeField] public KeyCode resetMappingHotkey = KeyCode.R;
     [SerializeField] public KeyCode showTestPatternHotkey = KeyCode.O;
     [SerializeField] public KeyCode exitWithoutSavingHotkey = KeyCode.Escape;
+#endif
 
     [Range(0.01f, 0.5f)]
     public float magneticCornerDistance = 0.2f;
@@ -62,66 +72,196 @@ public class PerspectiveMappingCamera : MonoBehaviour
 
     [HideInInspector] public PerspectiveMappingDisplayConfig config;
 
-    void OnEnable() {
+    void OnEnable()
+    {
         _cam = GetComponent<Camera>();
-       
-        if(_cam.targetDisplay < Display.displays.Length) {
+
+        if (_cam.targetDisplay < Display.displays.Length)
+        {
             Display.displays[_cam.targetDisplay].Activate();
         }
 
         ResetInvariants();
         LoadInvariants();
         _currentInvariants = mappingInvariants;
+
+#if ENABLE_INPUT_SYSTEM
+        InitNewInputSystem();
+#endif
+    }
+    
+#if ENABLE_INPUT_SYSTEM
+    public void InitNewInputSystem()
+    {
+        var playerinput = GetComponent<PlayerInput>();
+        if (playerinput == null)
+            playerinput = this.gameObject.AddComponent<PlayerInput>();
+
+        if (inputActions == null)
+        {
+            Debug.LogError("[PerspectiveMapping] No Input Action Asset set in the inspector. You can put the one from the package in Packages/PerspectiveMapping/PerspectiveMappingInputActions.inputactions");
+            return;
+        }
+        playerinput.actions = inputActions;
+    }
+#endif
+
+    public void OnInteractable()
+    {
+        #if !UNITY_EDITOR
+            if(GetMouseCurrentDisplay() != _cam.targetDisplay)
+                return;
+        #endif
+
+        if (interactable)
+        {
+            _isFollowingMouse = false;
+            SaveInvariants();
+        }
+        interactable = !interactable;
+    }
+
+    public void OnTestPattern()
+    {
+        if (!interactable) return;
+
+    #if !UNITY_EDITOR
+            if(GetMouseCurrentDisplay() != _cam.targetDisplay)
+                return;
+    #endif
+        showTestPatternTexture = !showTestPatternTexture;
+    }
+
+    public void OnResetMapping()
+    {
+        if (!interactable) return;
+
+        #if !UNITY_EDITOR
+            if(GetMouseCurrentDisplay() != _cam.targetDisplay)
+                return;
+        #endif
+        ResetInvariants();
+    }
+
+    public void OnExitWithoutSaving()
+    {
+        if (!interactable) return;
+        
+        handles.SelectNone();
+        _isFollowingMouse = false;
+        interactable = false;
+        LoadInvariants();
+    }
+
+    public void OnNextHandle()
+    {
+        if (!interactable) return;
+
+        int currentIndex = Array.IndexOf(handles.all, handles.current);
+        
+        // Tab to go forwards
+        currentIndex++;
+        if (currentIndex >= handles.all.Length)
+            currentIndex = 0;
+        
+        handles.SelectHandle(currentIndex + 1);
+    }
+
+    public void OnPreviousHandle()
+    {
+        if (!interactable) return;
+
+        int currentIndex = Array.IndexOf(handles.all, handles.current);
+
+        // Shift + Tab to go backwards
+        currentIndex--;
+        if (currentIndex < 0)
+            currentIndex = handles.all.Length - 1;
+
+        handles.SelectHandle(currentIndex + 1);
+    }
+
+
+    public void OnSelectHandle1()
+    {
+        if (!interactable) return;
+
+        if (handles.current == handles.targets[0])
+            handles.SelectNone();
+        else
+            handles.SelectHandle(1);
+    }
+
+    public void OnSelectHandle2()
+    {
+        if (!interactable) return;
+
+        if (handles.current == handles.targets[1])
+            handles.SelectNone();
+        else
+            handles.SelectHandle(2);
+    }
+
+    public void OnSelectHandle3()
+    {
+        if (!interactable) return;
+
+        if (handles.current == handles.targets[2])
+            handles.SelectNone();
+        else
+            handles.SelectHandle(3);
+    }
+
+    public void OnSelectHandle4()
+    {
+        if (!interactable) return;
+
+        if (handles.current == handles.targets[3])
+            handles.SelectNone();
+        else
+            handles.SelectHandle(4);
+    }
+
+    public void OnSelectHandleCenter()
+    {
+        if (!interactable) return;
+
+        if (handles.current == handles.center)
+            handles.SelectNone();
+        else
+            handles.SelectHandle(5);
     }
 
     void Update() {
-
-        if(Input.GetKeyDown(mappingModeHotkey)) {
-            #if !UNITY_EDITOR
-                if(GetMouseCurrentDisplay() != _cam.targetDisplay)
-                    return;
-            #endif
-
-            if(interactable) {
-                _isFollowingMouse = false;
-                SaveInvariants();
-            }
-            interactable = !interactable;
-        }
-
-        if(Input.GetKeyDown(resetMappingHotkey)) {
-            #if !UNITY_EDITOR
-                if(GetMouseCurrentDisplay() != _cam.targetDisplay)
-                    return;
-            #endif
-            ResetInvariants();
-        }
-
-        if(Input.GetKeyDown(showTestPatternHotkey)) {
-            #if !UNITY_EDITOR
-                if(GetMouseCurrentDisplay() != _cam.targetDisplay)
-                    return;
-            #endif
-            showTestPatternTexture = !showTestPatternTexture;
-        }
 
         if (mappingInvariants != _currentInvariants) {
             _currentInvariants = mappingInvariants;
             ResetInvariants();
         }
 
-        if(!interactable)
-            return;
 
+#if !ENABLE_INPUT_SYSTEM
         //Check keyboard
-        UpdateKeyboard();
+        UpdateLegacyKeyboard();
+#endif
+
+        //Check mouse
+        UpdateMouse();
+    }
+
+    public void UpdateMouse()
+    {
+        if (!interactable)
+            return;
 
         //Check mouse
         handles.magneticDistance = this.magneticCornerDistance;
-
+#if ENABLE_INPUT_SYSTEM
+        Vector3 mainMousePos = Mouse.current.position.ReadValue();
+#else
         Vector3 mainMousePos = Input.mousePosition;
+#endif
         Vector3 relMousePos = Display.RelativeMouseAt( mainMousePos ); 
-        
         int hoveredDisplay = (int) relMousePos.z;
         if( hoveredDisplay == _cam.targetDisplay ) {
             _multiDisplayOffset = mainMousePos - relMousePos;
@@ -130,22 +270,30 @@ public class PerspectiveMappingCamera : MonoBehaviour
 
         #if UNITY_EDITOR
             _multiDisplayOffset = Vector3.zero;
-        #endif
+#endif
 
-        #if !UNITY_EDITOR
+#if !UNITY_EDITOR
             if( GetMouseCurrentDisplay() != _cam.targetDisplay ) {
                 handles.SelectNone();
                 return;
             }
-        #endif
+#endif
+
+#if ENABLE_INPUT_SYSTEM
+        bool mouseBtn0Down = Mouse.current.leftButton.wasPressedThisFrame;
+        bool mouseBtn0Up = Mouse.current.leftButton.wasReleasedThisFrame;
+#else
+        bool mouseBtn0Down = Input.GetMouseButtonDown(0);
+        bool mouseBtn0Up = Input.GetMouseButtonUp(0);
+#endif
 
         // listen for mouse to change state
-        bool mouseButtonJustPressed = !_isFollowingMouse && Input.GetMouseButtonDown(0);
-        bool mouseButtonJustReleased = _isFollowingMouse && Input.GetMouseButtonUp(0);
+        bool mouseButtonJustPressed = !_isFollowingMouse && mouseBtn0Down;
+        bool mouseButtonJustReleased = _isFollowingMouse && mouseBtn0Up;
 
         if (mouseButtonJustPressed) {
             _isFollowingMouse = true;
-            var mousePos = _cam.ScreenToViewportPoint(Input.mousePosition - _multiDisplayOffset);
+            var mousePos = _cam.ScreenToViewportPoint(mainMousePos - _multiDisplayOffset);
             mousePos = remap(mousePos, 0f, 1f, -1f, 1f);
             handles.SelectClosestHandle(mousePos);
         }
@@ -156,7 +304,7 @@ public class PerspectiveMappingCamera : MonoBehaviour
 
         if (handles.current.type != HandleType.None) {
             if (_isFollowingMouse) {
-                var mousePos = _cam.ScreenToViewportPoint(Input.mousePosition - _multiDisplayOffset);
+                var mousePos = _cam.ScreenToViewportPoint(mainMousePos - _multiDisplayOffset);
                 mousePos = remap(mousePos, 0f, 1f, -1f, 1f);
                 // constrain handle to screen
                 Vector3 clampedMousePos = MathTools.Clamp(mousePos, -1.0f, 1.0f);
@@ -164,83 +312,90 @@ public class PerspectiveMappingCamera : MonoBehaviour
             }
             else {
                 // Translate.
-                Vector2 delta = new Vector2( Input.GetAxisRaw( "Horizontal" ), Input.GetAxisRaw( "Vertical" ) * _cam.aspect ) * 0.1f;
-                if (Input.GetKey( KeyCode.LeftShift ) || Input.GetKey( KeyCode.RightShift ) ) delta *= 10;
-                else if (Input.GetKey( KeyCode.LeftControl ) || Input.GetKey( KeyCode.RightControl ) ) delta *= 0.2f;
+#if ENABLE_INPUT_SYSTEM
+                Vector2 delta = Vector2.zero;
+                if (Keyboard.current.leftArrowKey.isPressed) delta.x -= 1;
+                if (Keyboard.current.rightArrowKey.isPressed) delta.x += 1;
+                if (Keyboard.current.downArrowKey.isPressed) delta.y -= 1;
+                if (Keyboard.current.upArrowKey.isPressed) delta.y += 1;
+                delta.y *= _cam.aspect;
+                delta *= 0.1f;
+                
+                if (Keyboard.current.shiftKey.isPressed) delta *= 10;
+                else if (Keyboard.current.ctrlKey.isPressed) delta *= 0.2f;
+#else
+                Vector2 delta = new Vector2( Input.GetAxisRaw( "Horizontal" ), Input.GetAxisRaw( "Vertical" ) * _cam.aspect)* 0.1f;
+                if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) delta *= 10;
+                else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) delta *= 0.2f;
+#endif
+
                 handles.SetPosition(handles.current, handles.current.GetPosition() + delta * Time.deltaTime);
             }
         }
     }
-
-    public void UpdateKeyboard()
+    
+#if !ENABLE_INPUT_SYSTEM
+    public void UpdateLegacyKeyboard()
     {
+        if (Input.GetKeyUp(mappingModeHotkey))
+        {
+            OnInteractable();
+        }
+
+        if (!interactable)
+            return;
+
+        if (Input.GetKeyUp(resetMappingHotkey))
+        {
+            OnResetMapping();
+        }
+
+        if (Input.GetKeyUp(showTestPatternHotkey))
+        {
+            OnTestPattern();
+        }
+
+        if (Input.GetKeyUp(exitWithoutSavingHotkey))
+        {
+            OnExitWithoutSaving();
+        }
+
         //Using number keys to select handles
         if (Input.GetKeyUp(KeyCode.Alpha1) || Input.GetKeyUp(KeyCode.Keypad1))
         {
-            if (handles.current == handles.targets[0])
-                handles.SelectNone();
-            else
-                handles.SelectHandle(1);
+            OnSelectHandle1();
         }
         else if (Input.GetKeyUp(KeyCode.Alpha2) || Input.GetKeyUp(KeyCode.Keypad2))
         {
-            if (handles.current == handles.targets[1])
-                handles.SelectNone();
-            else
-                handles.SelectHandle(2);
+            OnSelectHandle2();
         }
         else if (Input.GetKeyUp(KeyCode.Alpha3) || Input.GetKeyUp(KeyCode.Keypad3))
         {
-            if (handles.current == handles.targets[2])
-                handles.SelectNone();
-            else
-                handles.SelectHandle(3);
+            OnSelectHandle3();
         }
         else if (Input.GetKeyUp(KeyCode.Alpha4) || Input.GetKeyUp(KeyCode.Keypad4))
         {
-            if (handles.current == handles.targets[3])
-                handles.SelectNone();
-            else
-                handles.SelectHandle(4);
+            OnSelectHandle4();
         }
         else if (Input.GetKeyUp(KeyCode.Alpha5) || Input.GetKeyUp(KeyCode.Keypad5))
         {
-            if (handles.current == handles.center)
-                handles.SelectNone();
-            else
-                handles.SelectHandle(5);
+            OnSelectHandleCenter();
         }
 
         //Using tab to cycle through handles and shift tab to go backwards
         if (Input.GetKeyUp(KeyCode.Tab))
         {
-            int currentIndex = Array.IndexOf(handles.all, handles.current);
             if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
             {
-                // Shift + Tab to go backwards
-                currentIndex--;
-                if (currentIndex < 0)
-                    currentIndex = handles.all.Length - 1;
+                OnPreviousHandle();
             }
             else
             {
-                // Tab to go forwards
-                currentIndex++;
-                if (currentIndex >= handles.all.Length)
-                    currentIndex = 0;
+                OnNextHandle();
             }
-            handles.SelectHandle(currentIndex + 1);
-        }
-        
-        //Using escape to exit interactable mode and load previous invariants
-        if (Input.GetKeyUp(exitWithoutSavingHotkey))
-        {
-            handles.SelectNone();
-            _isFollowingMouse = false;
-            interactable = false;
-            LoadInvariants();
         }
     }
+#endif 
 
     void OnApplicationQuit() {
         SaveInvariants();
@@ -248,7 +403,8 @@ public class PerspectiveMappingCamera : MonoBehaviour
 
     // TODO?: keep transformation when switching invariants
     // requires to apply homography on new invariant coordinates
-    public void ResetInvariants() {
+    public void ResetInvariants()
+    {
         // Debug.Log("Reset Invariants");
         Vector2[] sources = new Vector2[4];
         switch (mappingInvariants)
@@ -267,17 +423,17 @@ public class PerspectiveMappingCamera : MonoBehaviour
 
                 //OpenGL coordinates
                 sources[1] = new Vector2(-a, 0); // left of ellipse
-                sources[0] = new Vector2( 0, b); // top of ellipse
-                sources[3] = new Vector2( a, 0); // right of ellipse
-                sources[2] = new Vector2( 0,-b); // bottom of ellipse
+                sources[0] = new Vector2(0, b); // top of ellipse
+                sources[3] = new Vector2(a, 0); // right of ellipse
+                sources[2] = new Vector2(0, -b); // bottom of ellipse
                 break;
 
             default: // MappingInvariants.Corners
                 //OpenGL coordinates
-                sources[1] = new Vector2(-1,-1);
+                sources[1] = new Vector2(-1, -1);
                 sources[0] = new Vector2(-1, 1);
-                sources[3] = new Vector2( 1, 1);
-                sources[2] = new Vector2( 1,-1);
+                sources[3] = new Vector2(1, 1);
+                sources[2] = new Vector2(1, -1);
                 break;
         }
 
